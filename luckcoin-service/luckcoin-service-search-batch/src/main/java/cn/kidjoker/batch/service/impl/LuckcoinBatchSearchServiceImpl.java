6a32b67759d78938453b06a9e8bc32125c709c91
Administrator <Administrator@PC-201710231914>
2017-12-14 14:26:58 +0800

    添加batch模块

---
+++ luckcoin-service/luckcoin-service-search-batch/src/main/java/cn/kidjoker/batch/service/impl/LuckcoinBatchSearchServiceImpl.java
@@ -0,0 +1,39 @@
+/*
+ * ================================================================
+ * Copyright 2008-2017 Everelegance.
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Everelegance Corp. Ltd, ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Everelegance.
+ * 
+ * OPay项目组
+ *
+ * ================================================================
+ *  创建人: jinzhijie
+ *	创建时间: 2017年12月14日 下午1:08:41
+ */
+package cn.kidjoker.batch.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import cn.kidjoker.batch.service.LuckcoinBatchSearchService;
+
+/**
+ * <p>
+ * 
+ * </P>
+ * @author jinzhijie
+ * @creatTime 2017年12月14日 下午1:08:41
+ */
+@Service("LuckcoinSearchBatchJob")
+public class LuckcoinBatchSearchServiceImpl implements LuckcoinBatchSearchService {
+
+	@Override
+	public void execute() throws Exception {
+		System.out.println("123");
+	}
+	
+}
