423f8afe7c08351f0bb6c78f5313da93a9cece1c
kidjoker <13621952184@163.com>
2017-12-17 12:14:20 +0800

    添加测试类 junit版本提升为4.11

---
+++ luckcoin-service/luckcoin-service-core-service-impl/src/test/java/cn/kidjoker/core/test/CoinTypeTest.java
@@ -0,0 +1,15 @@
+/** work for life!
+ * 
+ */
+package cn.kidjoker.core.test;
+
+import cn.kidjoker.common.test.AbstractTest;
+
+/**
+ * @author kidjoker
+ *
+ * @date 2017年12月17日 
+ */
+public class CoinTypeTest extends AbstractTest {
+
+}
